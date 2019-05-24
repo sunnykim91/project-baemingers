a8da5b7f4c18340d132e20ad7c9aaf40474cebca
sunnykim91 <tjsgnl2002@gmail.com>
2019-05-24 13:36:37 +0900

    series add

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function ($) {
+  //set here the speed to change the slides in the carousel
+  $('#myCarousel').carousel({
+    interval: 5000
+  });
+
+  //Loads the html to each slider. Write in the "div id="slide-content-x" what you want to show in each slide
+  $('#carousel-text').html($('#slide-content-0').html());
+
+  //Handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click(function () {
+    var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+    var id = parseInt(id);
+    $('#myCarousel').carousel(id);
+  });
+
+  $('#myCarousel').on('slid.bs.carousel', function (e) {
+    var id = $('.item.active').data('slide-number');
+    $('[id^=carousel-selector-]').removeClass('redhover');
+    $('#carousel-selector-' + id).addClass('redhover');
+    $('#carousel-text').html($('#slide-content-' + id).html());
+  });
+
+});
